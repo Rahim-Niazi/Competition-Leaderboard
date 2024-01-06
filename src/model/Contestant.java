216bfb59e8674e8228777d04103d94e13b6a513b
rahim-niazi <rahimhassanniazi@gmail.com>
2024-01-06 16:51:44 +0500

    attributes

---
+++ src/model/Contestant.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Arrays;
+
+public class Contestant {
+    private int ContestantNumber;
+    private String name;
+
+    private int age;
+    private String gender;
+    private String country;
+    private int[] scores;
+}
