168db2e08ae7d01c7aacf365b515faeac84ba8bf
rahim-niazi <rahimhassanniazi@gmail.com>
2024-01-06 19:08:49 +0500

    More functions relating to contestants was added as well as a function to write contestants to a CSV file.

--- src/model/ContestantList.java
+++ src/model/ContestantList.java
@@ -10,13 +10,67 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ContestantList
-{
+public class ContestantList {
     private List<Contestant> contestants;
+
     public ContestantList() {
         contestants = new ArrayList<>();
     }
-    public void addContestant(int ContestantNumber, String name, Contestant.Category category, Contestant.Level level, int[] scores, int age, String gender, String country)
-    {
-        Contestant contestant = new Contestant(ContestantNumber, name, level, category, scores, age, gender, country);contestants.add(contestant);
+
+    public void addContestant(int ContestantNumber, String name, Contestant.Category category, Contestant.Level level, int[] scores, int age, String gender, String country) {
+        Contestant contestant = new Contestant(ContestantNumber, name, level, category, scores, age, gender, country);
+        contestants.add(contestant);
     }
+
+    public Contestant getContestantByNumber(int ContestantNumber) {
+        for (Contestant contestant : contestants) {
+            if (contestant.getContestantNumber() == ContestantNumber) {
+                return contestant;
+            }
+        }
+        return null;
+    }
+
+    public void removeContestant(int ContestantNumber) {
+        Contestant contestantToRemove = null;
+        for (Contestant contestant : contestants) {
+            if (contestant.getContestantNumber() == ContestantNumber) {
+                contestantToRemove = contestant;
+                break;
+            }
+        }
+        if (contestantToRemove != null) {
+            contestants.remove(contestantToRemove);
+        }
+    }
+
+    public List<Contestant> getAllContestants() {
+        return contestants;
+    }
+
+    public Contestant getWinner() {
+        return contestants.stream()
+                .max((c1, c2) -> Double.compare(c1.getOverallScore(), c2.getOverallScore()))
+                .orElse(null);
+    }
+
+    public void writeContestantsToCSV(String filePath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (Contestant contestant : contestants) {
+                String line = String.format("%d,%s,%s,%s,%d,%s,%s,%s",
+                        contestant.getContestantNumber(),
+                        contestant.getName(),
+                        contestant.getCategory(),
+                        contestant.getLevel(),
+                        contestant.getAge(),
+                        contestant.getGender(),
+                        contestant.getCountry(),
+                        String.join(",", Arrays.stream(contestant.getScores()).mapToObj(String::valueOf).toArray(String[]::new)));
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
