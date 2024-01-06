778940b8633645b91660619209e6d92c1c9d4e13
rahim-niazi <rahimhassanniazi@gmail.com>
2024-01-06 21:20:56 +0500

    Controller class was made.

---
+++ src/Controller/Controller.java
@@ -0,0 +1,10 @@
+package Controller;
+import view.frontend;
+import model.ContestantList;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller {
+}
