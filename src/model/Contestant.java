798462f3ed2e329570fa8b843a8f784096da87a8
rahim-niazi <rahimhassanniazi@gmail.com>
2024-01-06 17:09:13 +0500

    Setters and Getters were added.

--- src/model/Contestant.java
+++ src/model/Contestant.java
@@ -21,3 +21,40 @@ public Contestant(int ContestantNumber, String name, int[] scores, int age, Stri
     this.gender = gender;
     this.country = country;
 }
+
+public int getContestantNumber() {
+    return ContestantNumber;
+}
+public String getName() {
+    return name;
+}
+public int getAge() {
+    return age;
+}
+public String getGender() {
+    return gender;
+}
+public String getCountry() {
+    return country;
+}
+public int[] getScores() {
+    return scores;
+}
+public double getOverallScore() {return 5;}
+
+
+public void setName(String newName) {
+    this.name = newName;
+}
+public void setAge(int newAge) {
+    this.age = newAge;
+}
+public void setGender(String newGender) {
+    this.gender = newGender;
+}
+public void setCountry(String newCountry) {
+    this.country = newCountry;
+}
+public void setScores(int[] newScores) {
+    this.scores = newScores;
+}
