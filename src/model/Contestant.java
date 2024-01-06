3f1772cb74377d352dd2a9e800f9c8936ee327e9
rahim-niazi <rahimhassanniazi@gmail.com>
2024-01-06 16:59:23 +0500

    Constructor was added.

--- src/model/Contestant.java
+++ src/model/Contestant.java
@@ -2,12 +2,22 @@ package model;
 
 import java.util.Arrays;
 
-public class Contestant {
+public class Contestant
+{
     private int ContestantNumber;
     private String name;
-
     private int age;
     private String gender;
     private String country;
     private int[] scores;
+}
+
+public Contestant(int ContestantNumber, String name, int[] scores, int age, String gender, String country)
+{
+    this.ContestantNumber = ContestantNumber;
+    this.name = name;
+    this.scores = scores;
+    this.age = age;
+    this.gender = gender;
+    this.country = country;
 }
