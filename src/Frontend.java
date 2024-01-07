661eee6520445fd78c1abb1830e55cfc13ae16fb
rahim-niazi <rahimhassanniazi@gmail.com>
2024-01-07 15:19:59 +0500

    Frontend class was created which we will use for implementing GUI functions.

---
+++ src/Frontend.java
@@ -0,0 +1,14 @@
+package view;
+import model.Contestant;
+import model.ContestantList;
+import model.User;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+public class Frontend {
+}
