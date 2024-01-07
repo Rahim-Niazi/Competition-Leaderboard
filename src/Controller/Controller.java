4a9f285ef089f1037fefe87bbdba1e9a3a5439fa
rahim-niazi <rahimhassanniazi@gmail.com>
2024-01-07 14:26:45 +0500

    Constructors were made that will be used later in the GUI.

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -6,5 +6,21 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Controller {
+public class Controller
+{
+    private frontend view;
+    private ContestantList model;
+    public Controller(frontend view, ContestantList model)
+    {
+        this.view = view;
+        this.model = model;
+
+        view.getUpdateButton().addActionListener(new UpdateButtonListener());
+        view.getEditButton().addActionListener(new EditButtonListener());
+        view.getRemoveButton().addActionListener(new RemoveButtonListener());
+        view.getReportButton().addActionListener(new ReportButtonListener());
+        view.getShortButton().addActionListener(new ShortButtonListener());
+        view.getAddContestantButton().addActionListener(new AddContestantButtonListener());
+        view.getSearchButton().addActionListener(new SearchButtonListener());
+    }
 }
