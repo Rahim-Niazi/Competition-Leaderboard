2a3115ed03ea75ec8c3b2af5f846aa0aacc83e8b
rahim-niazi <rahimhassanniazi@gmail.com>
2024-01-07 15:06:22 +0500

    Function for Showing Selection Panel was added.

--- src/Manager.java
+++ src/Manager.java
@@ -8,4 +8,34 @@ import java.awt.*;
 
 public class Manager
 {
+    private JFrame selectionFrame;
+
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(() -> new Manager().showSelectionPanel());
+    }
+
+    private void showSelectionPanel()
+    {
+        selectionFrame = new JFrame("Role Selection");
+        selectionFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel selectionPanel = new JPanel();
+        selectionPanel.setLayout(new GridLayout(2, 1));
+
+        JButton staffButton = new JButton("Staff");
+        JButton ContestantButton = new JButton("Contestant");
+
+        staffButton.addActionListener(e -> openCompetitionGUI("staff"));
+        ContestantButton.addActionListener(e -> openCompetitionGUI("Contestant"));
+
+        selectionPanel.add(staffButton);
+        selectionPanel.add(ContestantButton);
+
+        selectionFrame.add(selectionPanel);
+        selectionFrame.setSize(400, 400);
+        selectionFrame.setLocationRelativeTo(null);
+        selectionFrame.setVisible(true);
+    }
+
 }
